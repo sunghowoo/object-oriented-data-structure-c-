02d9e12e3a798ae144000dc862605f33a996954f
sunghowoo <dkdltu1111@gmail.com>
2022-01-30 14:41:39 +0900

    week3

--- week2/cpp-heapMemory/main.cpp
+++ week2/cpp-heapMemory/main.cpp
@@ -11,15 +11,15 @@ int main() {
   int *numPtr = new int;
 
   std::cout << "*numPtr: " << *numPtr << std::endl;
-  std::cout << " numPtr: " <<  numPtr << std::endl; //heap addess going up from lower number 
-  std::cout << "&numPtr: " << &numPtr << std::endl; //stack address
+  std::cout << " numPtr: " <<  numPtr << std::endl;
+  std::cout << "&numPtr: " << &numPtr << std::endl;
 
   *numPtr = 42;
   std::cout << "*numPtr assigned 42." << std::endl;
 
   std::cout << "*numPtr: " << *numPtr << std::endl;
-  std::cout << " numPtr: " <<  numPtr << std::endl;// the memorry
-  std::cout << "&numPtr: " << &numPtr << std::endl;// address of * numPtr
+  std::cout << " numPtr: " <<  numPtr << std::endl;
+  std::cout << "&numPtr: " << &numPtr << std::endl;
 
   return 0;
 }
